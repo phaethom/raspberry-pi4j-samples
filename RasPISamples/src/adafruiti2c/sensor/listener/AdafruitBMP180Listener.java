80b0d136fbf4da99929e017261a5ad313c9a150e
Olivier LeDiouris <olivier.lediouris@oracle.com>
2015-05-21 19:17:14 -0700

    Building RasPISamples

--- RasPISamples/src/adafruiti2c/sensor/listener/AdafruitBMP180Listener.java
+++
@@ -1,9 +0,0 @@
-package adafruiti2c.sensor.listener;
-
-import java.util.EventListener;
-import ocss.nmea.api.NMEAEvent;
-
-public abstract class AdafruitBMP180Listener implements EventListener
-{
-  public void dataDetected(NMEAEvent e) {}
-}
