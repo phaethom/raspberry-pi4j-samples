89a33a9723c180cfa2e901b0a04336214d715988
Olivier LeDiouris <olivier.lediouris@oracle.com>
2015-06-06 18:26:34 -0700

    Scala-ized

--- Scala.101/src/Scala_101.scala
+++ Scala.101/src/Scala_101.scala
@@ -1,19 +1,21 @@
 import adafruiti2c.sensor.AdafruitBMP180
+import com.pi4j.system.SystemInfo
 
 object Scala_101 {
   def main(args: Array[String]) {
     println("Hello, Scala world!")
-    val bmp180 = new AdafruitBMP180();
+    val bmp180 = new AdafruitBMP180
     try
     {
-      val temp  = bmp180.readTemperature()
-      val press = bmp180.readPressure() / 100
+      val temp  = bmp180.readTemperature
+      val press = bmp180.readPressure / 100
+      println("CPU Temperature   :  " + SystemInfo.getCpuTemperature + "\272C")
       println("Temp:" + temp + "\272C, Press:" + press + " hPa")
     }
     catch
     {
       case ex: Exception => {
-        println(ex.toString())
+        println(ex.toString)
       }
     }
   }
